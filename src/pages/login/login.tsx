47beb89a6ce1ddc0cc784372e21461bd7cd5b956
Administrator <admin@example.com>
2025-11-06 21:35:04 -0600

    Style: Add login styles

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,33 +1,43 @@
+import { Button } from 'primereact/button';
+import { InputText } from 'primereact/inputtext';
+import { Password } from 'primereact/password';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './login.css';
 
-
-
 function Login() {
   const navigate = useNavigate();
+  const [user, setUser] = useState('');
+  const [password, setPassword] = useState('');
 
   const onLoginClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     navigate('/post');
   }
   return (
-    <article className='login'>
-      <header>
-        <h1>Soto transportes Post</h1>
-      </header>
-      <section>
-        <label>
-          Username
-          <input type="text" />
-        </label>
-        <label>
-          Password
-          <input type="password" />
-        </label>
-        <button onClick={onLoginClick}>
-          Login
-        </button>
-      </section>
-    </article>
+    <div className="login--body">
+      <article className="login">
+        <header>
+          <h1>Soto transportes Login</h1>
+        </header>
+        <section>
+          <label className='form-control'>
+            <span>User name</span>
+            <InputText className='input-field' value={user} onChange={(e) => setUser(e.target.value)} />
+          </label>
+          <label className='form-control'>
+            <span>Password</span>
+            <Password
+              className='input-field'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              feedback={false}
+              tabIndex={1}
+            />
+          </label>
+          <Button onClick={onLoginClick} label='Login' />
+        </section>
+      </article>
+    </div>
   );
 }
 export default Login
